99412699c9f43eab3385edb7fed83336f1f2bf9b
NathanChase22 <ntc477@utexas.edu>
2025-04-17 20:53:24 -0500

    Update: Originally API code only created a socket for single time use. Now we have a client class that allows for multiple messages now

--- api.py
+++ api.py
@@ -1,26 +1,56 @@
 import socket
 import time
 
-def send_message(message: str, host='localhost', port=3000):
-    """Send a message to the ROS 2 TCP bridge node"""
-    try:
-        with socket.create_connection((host, port), timeout=5) as sock:
-            print(f"Connected to {host}:{port}")
-            sock.sendall(message.encode())
-            print(f"Sent: {message}")
+class ROSBridgeClient:
+    def __init__(self, host='localhost', port=3000):
+        self.host = host
+        self.port = port
+        self.sock = None
+    
+    def connect(self):
 
+        try:
+            self.sock = socket.create_connection((self.host, self.port), timeout=5)
+            print(f"Connected to {self.host}:{self.port}")
+        except ConnectionRefusedError:
+            print(f"Could not connect to {self.host}:{self.port}. Is the container running?")
+        except Exception as e:
+            print(f"Error: {e}")
+            raise
+    
+    def send_message(self, message: str):
+        if not self.sock:
+            self.connect()
+
+        #try sending a message
+        try:
+            self.sock.sendall(message.encode())
+            print(f"Sent: {message}")
+            
             # Optionally receive a response (1 KB max)
-            response = sock.recv(1024).decode()
+            response = self.sock.recv(1024).decode()
             print(f"Received: {response.strip()}")
 
-    except ConnectionRefusedError:
-        print(f"Could not connect to {host}:{port}. Is the container running?")
-    except Exception as e:
-        print(f"Error: {e}")
+        #raise any exception and remove socket
+        except Exception as e:
+            print(f"Error: {e}")
+            self.sock = None #reconnect on next message
+            raise
+
+    def close(self):
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+
 
 if __name__ == "__main__":
-    while True:
-        msg = input("Enter a message to send (or 'exit'): ")
-        if msg.lower() == 'exit':
-            break
-        send_message(msg)
+    client = ROSBridgeClient()
+    try:
+        client.connect()
+        while True:
+            msg = input("Enter a message to send (or 'exit'): ")
+            if msg.lower() == 'exit':
+                break
+            client.send_message(msg)
+    finally:
+        client.close()
